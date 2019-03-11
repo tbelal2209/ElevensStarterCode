088616673efe4d684555f6dd2ddc23f2505804bc
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2019-03-11 10:44:50 -0400

    Update ElevensBoard.java

--- src/Activity9/ElevensBoard.java
+++ src/Activity9/ElevensBoard.java
@@ -49,13 +49,20 @@ public class ElevensBoard extends Board {
      * In Elevens, the legal groups are (1) a pair of non-face cards
      * whose values add to 11, and (2) a group of three cards consisting of
      * a jack, a queen, and a king in some order.
+     *
      * @param selectedCards the list of the indices of the selected cards.
      * @return true if the selected cards form a valid group for removal;
-     *         false otherwise.
+     * false otherwise.
      */
     @Override
     public boolean isLegal(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        if (selectedCards.size() == 2) {
+            return containsPairSum11(selectedCards);
+        } else if (selectedCards.size() == 3) {
+            return containsJQK(selectedCards);
+        }
+        return false;
     }
 
     /**
@@ -63,35 +70,77 @@ public class ElevensBoard extends Board {
      * In Elevens, there is a legal play if the board contains
      * (1) a pair of non-face cards whose values add to 11, or (2) a group
      * of three cards consisting of a jack, a queen, and a king in some order.
+     *
      * @return true if there is a legal play left on the board;
-     *         false otherwise.
+     * false otherwise.
      */
     @Override
     public boolean anotherPlayIsPossible() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        List<Integer> cIndexes = cardIndexes();
+        if(containsPairSum11(cIndexes) || containsJQK(cIndexes))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
      * Check for an 11-pair in the selected cards.
+     *
      * @param selectedCards selects a subset of this board.  It is list
      *                      of indexes into this board that are searched
      *                      to find an 11-pair.
      * @return true if the board entries in selectedCards
-     *              contain an 11-pair; false otherwise.
+     * contain an 11-pair; false otherwise.
      */
     private boolean containsPairSum11(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        if (selectedCards.size() < 2) {
+            return false;
+        }
+        for (int i = 0; i < selectedCards.size() - 1; i++) {
+            for (int j = i + 1; j < selectedCards.size(); j++) {
+                if (cardAt(selectedCards.get(i)).pointValue() + cardAt(selectedCards.get(j)).pointValue() == 11) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
      * Check for a JQK in the selected cards.
+     *
      * @param selectedCards selects a subset of this board.  It is list
      *                      of indexes into this board that are searched
      *                      to find a JQK group.
      * @return true if the board entries in selectedCards
-     *              include a jack, a queen, and a king; false otherwise.
+     * include a jack, a queen, and a king; false otherwise.
      */
     private boolean containsJQK(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+
+        boolean isthereJack = false;
+        boolean isthereQueen = false;
+        boolean isthereKing = false;
+        if (selectedCards.size() < 3) {
+            return false;
+        }
+        for (int i = 0; i < selectedCards.size(); i++) {
+            if (cardAt(selectedCards.get(i)).rank() == "jack") {
+                isthereJack = true;
+            }
+            else if (cardAt(selectedCards.get(i)).rank() == "queen") {
+                isthereQueen = true;
+            }
+            else if (cardAt(selectedCards.get(i)).rank() == "king") {
+                isthereKing = true;
+            }
+        }
+        return (isthereJack && isthereQueen && isthereKing);
     }
 }
